6e1cf2c804dce69f9c16ec611572c65d70786dae
jiang_chunqiao <chunqiao_jiang@icloud.com>
2022-10-13 11:05:02 +0800

    reactive - 优化1 使用高阶函数处理get set

--- src/reactivity/reactive/reactive.ts
+++ src/reactivity/reactive/reactive.ts
@@ -1,19 +1,32 @@
 import { track, trigger } from "../effect/effect";
 
+function createGetter(isReadonly = false) {
+    return function get(target, key) {
+        const res = Reflect.get(target, key);
+            // TODO 收集依赖
+        if (!isReadonly) {
+            track(target, key);
+        }
+        return res;
+    }
+}
+function createSetter() {
+    return function set(target, key, value) {
+        const res = Reflect.set(target, key, value);
+        // TODO 依赖触发
+        trigger(target, key);
+        return res; 
+    }
+}
+const get = createGetter();
+const set = createSetter();
 export function reactive(raw) {
     return new Proxy(raw, {
-        get(target,key) {
-            const res = Reflect.get(target, key);
-            // TODO 收集依赖
-            track(target, key);
-            return res;
-        },
-        set(target,key, value) {
-            const res = Reflect.set(target, key, value);
-            // TODO 依赖触发
-            trigger(target, key);
-            return res;
-        }
+        // 每次调用reactive都会执行createGetter,createGetter 只要初始化时创建就可以了
+        // get: createGetter(), 
+        // set: createSetter()
+        get,
+        set
     })
 }
 /**
@@ -23,10 +36,7 @@ export function reactive(raw) {
  */
 export function readonly(raw) {
     return new Proxy(raw, {
-        get(target,key) {
-            const res = Reflect.get(target, key);
-            return res;
-        },
+        get: createGetter(true),
         set(target, key: any, value) {
             console.warn(`key ${key} set failed becauseof target is readonly: ${target}`); 
             return true;
